51b4275f650c84046ea12ee4225d28453a9968cc
agb1986 <abrolly1986@gmail.com>
2021-07-09 16:38:48 +0100

    Adding logger functionaility

--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,39 @@
-module.exports = class Logger {
-    
-    static debug(debugMessageObject) {
-        console.log(debugMessageObject);
-    }
+const chalk = require("chalk");
 
-    static info(infoMessageObject) {
-        console.log(infoMessageObject);
+const __getPrefix = () => {
+  const now = new Date();
+  const timestamp = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+  const currentProcess = process.pid;
+  return `[${timestamp}][${currentProcess}] -`;
+};
+
+module.exports = class Logger {
+  static debug(debugMessageObject) {
+    console.debug(chalk.green("DEBUG:"), __getPrefix(), debugMessageObject);
+  }
+
+  static info(infoMessageObject) {
+    console.info(chalk.green("INFO:"), __getPrefix(), infoMessageObject);
+  }
+
+  static warn(warnMessageObject) {
+    console.warn(chalk.yellow("WARN:"), __getPrefix(), warnMessageObject);
+  }
+
+  static error(errorMessageObject) {
+    console.error(chalk.red("ERROR:"), __getPrefix(), errorMessageObject);
+  }
+
+  static error(errorMessageObject, err) {
+    if (err) {
+      console.error(
+        chalk.red("ERROR:"),
+        __getPrefix(),
+        errorMessageObject,
+        err.stack
+      );
+    } else {
+      console.error(chalk.red("ERROR:"), __getPrefix(), errorMessageObject);
     }
-}
+  }
+};
--- test/logger.test.js
+++ test/logger.test.js
@@ -1,9 +1,24 @@
-const Logger = require('../src/logger');
+const Logger = require("../src/logger");
 
-test('Testing logger DEBUG', () => {
-    Logger.debug('Hello World!'); 
+test("Testing logger DEBUG", () => {
+  Logger.debug("Hello World, this is a DEBUG log!");
 });
 
-test('Testing logger INFO', () => {
-    Logger.info('Hello World!'); 
+test("Testing logger INFO", () => {
+  Logger.info("Hello World, this is a INFO log!");
+});
+
+test("Testing logger WARN", () => {
+  Logger.warn("Hello World, this is a WARN log!");
+});
+
+test("Testing logger ERROR", () => {
+  Logger.error("Hello World, this is a ERROR log!");
+});
+
+test("Testing logger ERROR", () => {
+  Logger.error(
+    "Hello World, this is a ERROR log!",
+    new Error("This is a test error!")
+  );
 });
